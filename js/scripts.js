88bf7157b09b14394bfeef1c4979d409ffd573a7
Jiwon Kang and Kaz Matthews <kaz@>
2017-01-10 13:43:38 -0800

    add definitions to words and extend click functions to cover all 19 words.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,76 @@ $(document).ready(function() {
     $("#1 .showing").toggle();
     $("#1 .hidden").toggle();
   });
+  $("li#2").click(function() {
+    $("#2 .showing").toggle();
+    $("#2 .hidden").toggle();
+  });
+  $("li#3").click(function() {
+    $("#3 .showing").toggle();
+    $("#3 .hidden").toggle();
+  });
+  $("li#4").click(function() {
+    $("#4 .showing").toggle();
+    $("#4 .hidden").toggle();
+  });
+  $("li#5").click(function() {
+    $("#5 .showing").toggle();
+    $("#5 .hidden").toggle();
+  });
+  $("li#6").click(function() {
+    $("#6 .showing").toggle();
+    $("#6 .hidden").toggle();
+  });
+  $("li#7").click(function() {
+    $("#7 .showing").toggle();
+    $("#7 .hidden").toggle();
+  });
+  $("li#8").click(function() {
+    $("#8 .showing").toggle();
+    $("#8 .hidden").toggle();
+  });
+  $("li#9").click(function() {
+    $("#9 .showing").toggle();
+    $("#9 .hidden").toggle();
+  });
+  $("li#10").click(function() {
+    $("#10 .showing").toggle();
+    $("#10 .hidden").toggle();
+  });
+  $("li#11").click(function() {
+    $("#11 .showing").toggle();
+    $("#11 .hidden").toggle();
+  });
+  $("li#12").click(function() {
+    $("#12 .showing").toggle();
+    $("#12 .hidden").toggle();
+  });
+  $("li#13").click(function() {
+    $("#13 .showing").toggle();
+    $("#13 .hidden").toggle();
+  });
+  $("li#14").click(function() {
+    $("#14 .showing").toggle();
+    $("#14 .hidden").toggle();
+  });
+  $("li#15").click(function() {
+    $("#15 .showing").toggle();
+    $("#15 .hidden").toggle();
+  });
+  $("li#16").click(function() {
+    $("#16 .showing").toggle();
+    $("#16 .hidden").toggle();
+  });
+  $("li#17").click(function() {
+    $("#17 .showing").toggle();
+    $("#17 .hidden").toggle();
+  });
+  $("li#18").click(function() {
+    $("#18 .showing").toggle();
+    $("#18 .hidden").toggle();
+  });
+  $("li#19").click(function() {
+    $("#19 .showing").toggle();
+    $("#19 .hidden").toggle();
+  });
 });
